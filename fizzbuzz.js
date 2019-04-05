9205fc03c17062fb5abe87990e075f1e180b267d
snowpeech <snowpeech@gmail.com>
2019-04-04 20:48:57 -0500

    fizzbuzz complete

---
+++ fizzbuzz.js
@@ -0,0 +1,21 @@
+var number = 16;
+
+function fizzbuzz(num){
+    for (var i=0; i<=num; i++){
+        if (i%3==0 && i%5==0 ){
+            console.log("fizzbuzz ")
+        }
+
+        else if(i%3==0){
+            console.log("fizz")
+        }
+        else if(i%5==0){
+            console.log("buzz")
+        }
+        else {
+            console.log(i)
+        }
+    }
+}
+
+fizzbuzz(number);
